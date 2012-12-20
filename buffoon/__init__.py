f81d4e451259fd2a7538ac96370ab8cd2a5c9e22
Dmitry Ermolov <epdmitry@yandex.ru>
2012-12-21 03:42:40 +0400

    Proper mongodb connection for heroku.

--- buffoon/__init__.py
+++ buffoon/__init__.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import flask
 
 from flask.ext.mongoengine import MongoEngine
 
 app = flask.Flask(__name__)
 app.config['SECRET_KEY'] = 'qwerty'
-app.config['MONGODB_DB'] = 'buffoon_game'
+app.config['MONGODB_DB'] = os.environ.get('MONGOHQ_URL', 'buffoon_game')
 
 db = MongoEngine(app)
 
